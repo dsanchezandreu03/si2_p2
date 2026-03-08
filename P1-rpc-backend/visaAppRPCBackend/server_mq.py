11780924b844cb7686b9aaf2780c2cb4c4bbd8f0
Diego Sanchez <diego.sancheza@estudiante.uam.es>
2026-03-08 17:29:34 +0100

    server_mq corrected

--- P1-rpc-backend/visaAppRPCBackend/server_mq.py
+++ P1-rpc-backend/visaAppRPCBackend/server_mq.py
@@ -6,6 +6,7 @@ import django
 import pika
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visaSite.settings')
 
